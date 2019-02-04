4cbacb752f804b2cf1952421a0198eeef4b51f5a
Dmitry Boyko <dmitryboyko@ua.fm>
2019-02-04 18:40:41 +0200

    remove3

--- MDStool/src/main/java/datapackage/DataRepository.java
+++
@@ -1,8 +0,0 @@
-package datapackage;
-
-import org.springframework.data.repository.CrudRepository;
-
-//@Transactional
-public interface DataRepository extends CrudRepository<TaggingTask, Long> {
-
-}
